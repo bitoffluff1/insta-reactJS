b21f99aafe329bf61a545db516d8b66b5f1a13dd
bitoffluff1 <Lena_khmelnikova@mail.ru>
2019-08-24 21:30:18 +0300

    Добавилась серверная часть проекта Компонент с использованием хуков - counter Компонент с использованием функций жизненного цикла в реакте - timer Авторизация

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,33 @@
 import './assets/global.scss';
 
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import ReactDom from 'react-dom';
 
 import {Profile} from "./components/Profile";
 import {Gallery} from "./components/Gallery";
+import {Counter} from "./components/Counter";
+import {Timer} from "./components/Timer";
+import {Auth} from "./components/Auth";
 
 class App extends Component {
+    state = {visible: true};
+
+    handleToggleClick = () => {
+        this.setState(prevState => ({visible: !prevState.visible}))
+    };
+
     render() {
+        const {visible} = this.state;
+
         return (
-            <div>
+            <Fragment>
+                <button onClick={this.handleToggleClick}>Toggle</button>
+                {visible && <Timer/>}
+
+                <Counter/>
+
+                <Auth/>
+
                 <header>
                     <div className="container">
                         <Profile/>
@@ -20,7 +38,7 @@ class App extends Component {
                         <Gallery/>
                     </div>
                 </main>
-            </div>
+            </Fragment>
 
         );
     }
